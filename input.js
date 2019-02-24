fc0a07dde743d6d2538d6bee3699eae24427ce39
Sergey Rogatin <sergey_rogatin@epam.com>
2019-02-24 23:38:34 +0300

    добавили точки с запятой

--- input.js
+++ input.js
@@ -1,8 +1,8 @@
-let KEY_RIGHT = 68
-let KEY_LEFT = 65
-let KEY_DOWN = 83
-let KEY_UP = 87
-let KEY_SPACE = 32
+let KEY_RIGHT = 68;
+let KEY_LEFT = 65;
+let KEY_DOWN = 83;
+let KEY_UP = 87;
+let KEY_SPACE = 32;
 const ROTATION_SPEED = 0.05;
 
 function makeKey() {
@@ -10,20 +10,20 @@ function makeKey() {
         isDown: false,
         wentDown: false,
         wentUp: false,
-    }
+    };
 }
 
-let upKey = makeKey()
-let leftKey = makeKey()
-let downKey = makeKey()
-let rightKey = makeKey()
-let spaceKey = makeKey()
+let upKey = makeKey();
+let leftKey = makeKey();
+let downKey = makeKey();
+let rightKey = makeKey();
+let spaceKey = makeKey();
 
 function handleKeyDown(event, keyCode, key) {
     if (keyCode === event.keyCode) {
         if (!key.isDown) {
-            key.wentDown = true
-            key.isDown = true
+            key.wentDown = true;
+            key.isDown = true;
         }
     }
 }
@@ -31,8 +31,8 @@ function handleKeyDown(event, keyCode, key) {
 function handleKeyUp(event, keyCode, key) {
     if (keyCode === event.keyCode) {
         if (key.isDown) {
-            key.wentUp = true
-            key.isDown = false
+            key.wentUp = true;
+            key.isDown = false;
         }
     }
 }
