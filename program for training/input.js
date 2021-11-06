7c32638054e14b7a877793640303899fe07927c3
AlexeyRogatin <a-rogatin@mail.ru>
2021-11-06 14:25:49 +0300

    version with no input

--- program for training/input.js
+++ program for training/input.js
@@ -27,62 +27,62 @@ function correctValueStr(valueStr) {
     return correctedStr;
 }
 
-if (!isMobile) {
-    window.onmousemove = function onmousemove(event) {
-        const rect = canvas.getBoundingClientRect();
-        mouse.x = (event.clientX - rect.left) / canvas.clientWidth * canvas.width;
-        mouse.y = (event.clientY - rect.top) / canvas.clientHeight * canvas.height;
-    }
-    window.onmousedown = function onmousedown(event) {
-        mouse.wentDown = true;
-        mouse.isDown = true;
-    }
-    window.onmouseup = function onmouseup(event) {
-        mouse.wentUp = true;
-        mouse.isDown = false;
-    }
-    window.onkeydown = function onkeydown(event) {
-        if (writingIndex !== -1) {
-            let valueStr = data[writingIndex].targetPercent;
-            switch (event.key) {
-                case 'Backspace': {
-                    if (valueStr.length) {
-                        valueStr = valueStr.slice(0, -1);
-                    }
-                } break;
-                default: {
-                    if (valueStr * 100 % 10 === 0 && (event.keyCode >= 48 && event.keyCode <= 57 || ((event.key === '.' || event.key === ',') && valueStr.split('.').length === 1))) {
-                        valueStr += event.key;
-                    }
-                }
-            }
-            valueStr = correctValueStr(valueStr);
-            data[writingIndex].targetPercent = valueStr;
-        }
-    }
-} else {
-    const rect = canvas.getBoundingClientRect();
-    window.ontouchmove = function ontouchmove(event) {
-        if (mouse.id === event.changedTouches[0].identifier) {
-            mouse.x = (event.changedTouches[0].clientX - rect.left) / canvas.clientWidth * canvas.width;
-            mouse.y = (event.changedTouches[0].clientY - rect.top) / canvas.clientHeight * canvas.height;
-        }
-    }
-    window.ontouchstart = function ontouchstart(event) {
-        if (!(mouse.isDown || mouse.wentUp)) {
-            mouse.id = event.changedTouches[0].identifier;
+// if (!isMobile) {
+//     window.onmousemove = function onmousemove(event) {
+//         const rect = canvas.getBoundingClientRect();
+//         mouse.x = (event.clientX - rect.left) / canvas.clientWidth * canvas.width;
+//         mouse.y = (event.clientY - rect.top) / canvas.clientHeight * canvas.height;
+//     }
+//     window.onmousedown = function onmousedown(event) {
+//         mouse.wentDown = true;
+//         mouse.isDown = true;
+//     }
+//     window.onmouseup = function onmouseup(event) {
+//         mouse.wentUp = true;
+//         mouse.isDown = false;
+//     }
+//     window.onkeydown = function onkeydown(event) {
+//         if (writingIndex !== -1) {
+//             let valueStr = data[writingIndex].targetPercent;
+//             switch (event.key) {
+//                 case 'Backspace': {
+//                     if (valueStr.length) {
+//                         valueStr = valueStr.slice(0, -1);
+//                     }
+//                 } break;
+//                 default: {
+//                     if (valueStr * 100 % 10 === 0 && (event.keyCode >= 48 && event.keyCode <= 57 || ((event.key === '.' || event.key === ',') && valueStr.split('.').length === 1))) {
+//                         valueStr += event.key;
+//                     }
+//                 }
+//             }
+//             valueStr = correctValueStr(valueStr);
+//             data[writingIndex].targetPercent = valueStr;
+//         }
+//     }
+// } else {
+//     const rect = canvas.getBoundingClientRect();
+//     window.ontouchmove = function ontouchmove(event) {
+//         if (mouse.id === event.changedTouches[0].identifier) {
+//             mouse.x = (event.changedTouches[0].clientX - rect.left) / canvas.clientWidth * canvas.width;
+//             mouse.y = (event.changedTouches[0].clientY - rect.top) / canvas.clientHeight * canvas.height;
+//         }
+//     }
+//     window.ontouchstart = function ontouchstart(event) {
+//         if (!(mouse.isDown || mouse.wentUp)) {
+//             mouse.id = event.changedTouches[0].identifier;
 
-            mouse.wentDown = true;
-            mouse.isDown = true;
-            mouse.x = (event.changedTouches[0].clientX - rect.left) / canvas.clientWidth * canvas.width;
-            mouse.y = (event.changedTouches[0].clientY - rect.top) / canvas.clientHeight * canvas.height;
-        }
-    }
-    window.ontouchend = function ontouchend(event) {
-        mouse.wentUp = true;
-        mouse.isDown = false;
-    }
-}
+//             mouse.wentDown = true;
+//             mouse.isDown = true;
+//             mouse.x = (event.changedTouches[0].clientX - rect.left) / canvas.clientWidth * canvas.width;
+//             mouse.y = (event.changedTouches[0].clientY - rect.top) / canvas.clientHeight * canvas.height;
+//         }
+//     }
+//     window.ontouchend = function ontouchend(event) {
+//         mouse.wentUp = true;
+//         mouse.isDown = false;
+//     }
+// }
 
 function clearMouse() {
     mouse.wentDown = false;
